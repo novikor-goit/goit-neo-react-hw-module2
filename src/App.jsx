a858477b5f6d44570af930b2b11ffd9526b1a3d9
Max Novik <novik.kor@gmail.com>
2025-05-03 16:46:25 +0300

    init

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+const App = () => {
+  return (
+    <main>
+      <section>
+        <h1>Sip Happens Café</h1>
+        <p>Please leave your feedback about our service by selecting one of the options below.</p>
+      </section>
+    </main>
+  );
+};
+
+export default App;
